00a1630850996e19274d14314b5b3e36f82ca90a
roman10 <liuf0005@gmail.com>
2013-08-18 18:01:12 +0800

    update ffmpeg 2.0 to ffmpeg 2.0.1

--- android-ffmpeg-tutorial01/jni/Android.mk
+++ android-ffmpeg-tutorial01/jni/Android.mk
@@ -8,4 +8,4 @@ LOCAL_LDLIBS := -llog -ljnigraphics -lz
 LOCAL_SHARED_LIBRARIES := libavformat libavcodec libswscale libavutil
 
 include $(BUILD_SHARED_LIBRARY)
-$(call import-module,ffmpeg-2.0/android/arm)
+$(call import-module,ffmpeg-2.0.1/android/arm)
